9f05b152855bd20b713ae41016e16b6dbc74a389
Karon Lewin <karonlewin@gmail.com>
2018-11-16 17:20:25 -0200

    Refactoring the review's test, factories and some constants.

--- spec/factories/annotation.rb
+++ spec/factories/annotation.rb
@@ -4,5 +4,29 @@ FactoryBot.define do
   factory :annotation do
     title { Faker::RickAndMorty.character }
     content { Faker::RickAndMorty.quote }
+
+    factory :annotation_2_weekly_reviews_done do
+      after :create do |annotation|
+        2.times do
+          annotation.reviews.last.mark_as_done
+        end
+      end
+    end
+
+    factory :annotation_on_the_last_weekly_review do
+      after :create do |annotation|
+        (ReviewGenerator::WEEKLY_REVIEWS-1).times do
+          annotation.reviews.last.mark_as_done
+        end
+      end
+    end
+
+    factory :annotation_on_the_last_review_ever do
+      after :create do |annotation|
+        (ReviewGenerator::TOTAL_REVIEWS-1).times do
+          annotation.reviews.last.mark_as_done
+        end
+      end
+    end
   end
 end
--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.feature 'User at dashboard:' do
 
+  before(:all) do
+    annotation = create :annotation_2_weekly_reviews_done
+    byebug
+  end
+
 
   it 'User see all annotations that need to be revised' do
     annotation = create :annotation
