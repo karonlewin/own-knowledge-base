4b6ab9488c0d307fa1cda731ec86ba71798db575
Karon Lewin <karonlewin@gmail.com>
2018-11-16 12:10:40 -0200

    Preparing the ground for dashboard.

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -4,9 +4,11 @@ RSpec.feature 'User at dashboard:' do
 
 
   it 'User see all annotations that need to be revised' do
+    annotation = create :annotation
+
     visit dashboard_path
 
-    expect(page).to have_text(annotation)
+    expect(page).to have_text(annotation.title)
   end
 
 end
