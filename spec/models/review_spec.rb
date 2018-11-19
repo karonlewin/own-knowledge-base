4c6b76a4935132a4198ecdfdd7b4d47ba95353d2
Karon Lewin <karonlewin@gmail.com>
2018-11-19 20:28:27 -0200

    Working on Review's model, filtering pending annotations.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 describe Review do
 
-  context 'generating reviews' do
-    it 'generate a WEEKLY review after create a reviewable' do
+  context 'Generating reviews:' do
+    it 'generate a WEEKLY review after create a reviewable:' do
       new_annotation = build :annotation
 
       expect{new_annotation.save!}.to change(Review, :count).by(1)
       expect(Review.last.date.to_date).to eq((new_annotation.created_at + 1.weeks).to_date)
     end
 
-    it 'generate a WEEKLY review when a reviewable is being reviewed' do
+    it 'generate a WEEKLY review when a reviewable is being reviewed:' do
       annotation = create :annotation
 
       first_week_review = annotation.reviews.last
@@ -19,7 +19,7 @@ describe Review do
       expect(annotation.reviews.last.date.to_date).to eq((first_week_review.date + 1.weeks).to_date)
     end
 
-    it 'generate a MONTHLY review when a reviewable is being reviewed after the weekly period' do
+    it 'generate a MONTHLY review when a reviewable is being reviewed after the weekly period:' do
       annotation = create :annotation_on_the_last_weekly_review
 
       last_week_review = annotation.reviews.last
@@ -35,7 +35,7 @@ describe Review do
       expect(annotation.reviews.last.date.to_date).to eq((last_month_review.date + 1.months).to_date)
     end
 
-    it 'stop to geenerate reviews after the weekly+monthly period' do
+    it 'stop to geenerate reviews after the weekly+monthly period:' do
       annotation = create :annotation_on_the_last_review_ever
 
       last_review = annotation.reviews.last
