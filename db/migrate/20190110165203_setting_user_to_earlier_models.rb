d49b2c48c7ed82e677a3ce69defc5805912443ea
Karon Lewin <karonlewin@gmail.com>
2019-01-10 14:57:41 -0200

    Working on filtering categories by user while creating an annotation. Migrating other earlier models to be related with the old and only user.

---
+++ db/migrate/20190110165203_setting_user_to_earlier_models.rb
@@ -0,0 +1,13 @@
+class SettingUserToEarlierModels < ActiveRecord::Migration[5.1]
+  def change
+    user = User.first
+
+    Review.all.each do |review|
+      review.update(user: user)
+    end
+
+    Category.all.each do |category|
+      category.update(user: user)
+    end
+  end
+end
