49842f0f87956bbb8ec3b06986a82d59b18821e0
Karon Lewin <karonlewin@gmail.com>
2019-01-10 14:49:13 -0200

    Fixing earlier annotations to have a user defined.

---
+++ db/migrate/20190110164218_setting_user_to_earlier_annotations.rb
@@ -0,0 +1,8 @@
+class SettingUserToEarlierAnnotations < ActiveRecord::Migration[5.1]
+  def change
+    user = User.first
+    Annotation.all.each do |annotation|
+      annotation.update(user: user)
+    end
+  end
+end
