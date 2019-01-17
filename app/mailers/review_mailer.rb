e1522779748df898e4840cecf05427e799514e66
Karon Lewin <karonlewin@gmail.com>
2019-01-17 10:48:13 -0200

    Testing SendGrid + Schedule on Heroku #2.

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -2,10 +2,11 @@ class ReviewMailer < ApplicationMailer
   default from: 'notifications@own-knowledge-base.com'
 
   def today_reviews
-    users = User.all
-    users.each do |user|
+    # users = User.all
+    # users.each do |user|
+      user = User.first
       @reviews = Review.by_user_id(user.id).today_reviews
       mail(to: user.email, subject: 'Welcome to My Awesome Site')
-    end
+    # end
   end
 end
