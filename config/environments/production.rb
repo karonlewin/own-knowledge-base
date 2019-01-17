efb8ea8cfa46743a66dcae9587c7d1684ee56c69
Karon Lewin <karonlewin@gmail.com>
2019-01-17 11:47:55 -0200

    Testing SendGrid + Schedule on Heroku #3.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yourdomain.com',
+    :domain => 'own-knowlgedge-base.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
