0efd54572b0e8147a81f4817941bd16b47801ca0
Karon Lewin <karonlewin@gmail.com>
2019-03-06 15:15:32 -0300

    Fixing FactoryBot problem.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.secret_key_base = SecureRandom.hex(64)
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
