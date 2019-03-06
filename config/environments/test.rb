a530ce66c3506d6ed873c686260ebc4d4fd280e0
Karon Lewin <karonlewin@gmail.com>
2019-03-06 15:19:33 -0300

    Configure secret_key_base.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.secret_key_base = SecureRandom.hex(64)
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
